dedf839af6bfd0834997cba0fa8d49f7a6bafdf7
Rishika0101 <rishika.kumari@knoldus.com>
2023-02-16 18:18:16 +0530

    Firt commit

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello! Welcome to Knoldus!!");
+		System.out.println("This is my first project Using Jenkins freestyle. ");
+
+	}
+
+}
