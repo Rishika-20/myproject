ad2e43fa395e3afa0829fa0ce32d862ffdda934a
Rishika0101 <124979948+Rishika0101@users.noreply.github.com>
2023-02-20 18:36:46 +0530

    Update Rishika.java

--- Rishika.java
+++ Rishika.java
@@ -1,6 +1,6 @@
 public class Rishika{
   public static void main(String args[]){
     System.out.println("Hello! Welcome to the first Jenkins project.");
-    System.out.println("This file has been updated by Rishika-1852.");
+    System.out.println("This file has been updated by Rishika-1852. Thank you !");
   }
 }
